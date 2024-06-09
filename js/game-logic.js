205922a2cf0e951e19b740fe47ea164960494fa6
sifu-ewu <yotullah@gmail.com>
2024-06-09 20:17:11 +0600

    3 functions completed

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,4 +1,3 @@
-
 // All code should be written in this file.
 let playerOneMoveOneType;
 let playerOneMoveTwoType;
@@ -12,23 +11,41 @@ let playerOneMoveThreeValue;
 let playerTwoMoveOneValue;
 let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
-function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+function setPlayerMoves(
+    player,
+    moveOneType,
+    moveOneValue,
+    moveTwoType,
+    moveTwoValue,
+    moveThreeType,
+    moveThreeValue
+) {
     if (
-        (moveOneType === 'rock' || moveOneType === 'paper' || moveOneType === 'scissors') &&
-        (moveTwoType === 'rock' || moveTwoType === 'paper' || moveTwoType === 'scissors') &&
-        (moveThreeType === 'rock' || moveThreeType === 'paper' || moveThreeType === 'scissors') &&
-        (moveOneValue >= 1 && moveOneValue <= 99) &&
-        (moveTwoValue >= 1 && moveTwoValue <= 99) &&
-        (moveThreeValue >= 1 && moveThreeValue <= 99) && (moveOneValue + moveTwoValue + moveThreeValue <= 99)
+        (moveOneType === "rock" ||
+            moveOneType === "paper" ||
+            moveOneType === "scissors") &&
+        (moveTwoType === "rock" ||
+            moveTwoType === "paper" ||
+            moveTwoType === "scissors") &&
+        (moveThreeType === "rock" ||
+            moveThreeType === "paper" ||
+            moveThreeType === "scissors") &&
+        moveOneValue >= 1 &&
+        moveOneValue <= 99 &&
+        moveTwoValue >= 1 &&
+        moveTwoValue <= 99 &&
+        moveThreeValue >= 1 &&
+        moveThreeValue <= 99 &&
+        moveOneValue + moveTwoValue + moveThreeValue <= 99
     ) {
-        if (player === 'Player One') {
+        if (player === "Player One") {
             playerOneMoveOneType = moveOneType;
             playerOneMoveOneValue = moveOneValue;
             playerOneMoveTwoType = moveTwoType;
             playerOneMoveTwoValue = moveTwoValue;
             playerOneMoveThreeType = moveThreeType;
             playerOneMoveThreeValue = moveThreeValue;
-        } else if (player === 'Player Two') {
+        } else if (player === "Player Two") {
             playerTwoMoveOneType = moveOneType;
             playerTwoMoveOneValue = moveOneValue;
             playerTwoMoveTwoType = moveTwoType;
@@ -37,13 +54,15 @@ function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoV
             playerTwoMoveThreeValue = moveThreeValue;
         }
     } else {
-        return 'Invalid Input';
+        return "Invalid Input";
     }
 }
 
-
 function getRoundWinner(roundNumber) {
-    let playerOneMoveType, playerOneMoveValue, playerTwoMoveType, playerTwoMoveValue;
+    let playerOneMoveType,
+        playerOneMoveValue,
+        playerTwoMoveType,
+        playerTwoMoveValue;
 
     if (roundNumber === 1) {
         playerOneMoveType = playerOneMoveOneType;
@@ -61,59 +80,74 @@ function getRoundWinner(roundNumber) {
         playerTwoMoveType = playerTwoMoveThreeType;
         playerTwoMoveValue = playerTwoMoveThreeValue;
     }
-    if (playerOneMoveType && playerTwoMoveType && playerOneMoveValue && playerTwoMoveValue && roundNumber > 0 && roundNumber < 4) {
-
-    if (playerOneMoveType === 'rock' && playerTwoMoveType ==='scissors') {
-        return 'Player One';
-    } else if (playerOneMoveType === 'rock' && playerTwoMoveType === 'paper') {
-        return 'Player Two';
-    } else if (playerOneMoveType === 'scissors' && playerTwoMoveType === 'paper') {
-        return 'Player One';
-    } else if (playerOneMoveType === 'paper' && playerTwoMoveType === 'rock') {
-        return 'Player One';
-    }else if (playerOneMoveType === 'scissors' && playerTwoMoveType === 'rock') {
-        return 'Player Two';
-    } else if (playerOneMoveType === 'paper' && playerTwoMoveType === 'scissors') {
-        return 'Player Two';
-
-    }else if (playerOneMoveType === playerTwoMoveType) {
-        if (playerOneMoveValue > playerTwoMoveValue) {
-            return 'Player One';
-        } else if (playerOneMoveValue < playerTwoMoveValue) {
-            return 'Player Two';
-        } else {
-            return 'Tie';
+    if (
+        playerOneMoveType &&
+        playerTwoMoveType &&
+        playerOneMoveValue &&
+        playerTwoMoveValue &&
+        roundNumber > 0 &&
+        roundNumber < 4
+    ) {
+        if (playerOneMoveType === "rock" && playerTwoMoveType === "scissors") {
+            return "Player One";
+        } else if (playerOneMoveType === "rock" && playerTwoMoveType === "paper") {
+            return "Player Two";
+        } else if (
+            playerOneMoveType === "scissors" &&
+            playerTwoMoveType === "paper"
+        ) {
+            return "Player One";
+        } else if (playerOneMoveType === "paper" && playerTwoMoveType === "rock") {
+            return "Player One";
+        } else if (
+            playerOneMoveType === "scissors" &&
+            playerTwoMoveType === "rock"
+        ) {
+            return "Player Two";
+        } else if (
+            playerOneMoveType === "paper" &&
+            playerTwoMoveType === "scissors"
+        ) {
+            return "Player Two";
+        } else if (playerOneMoveType === playerTwoMoveType) {
+            if (playerOneMoveValue > playerTwoMoveValue) {
+                return "Player One";
+            } else if (playerOneMoveValue < playerTwoMoveValue) {
+                return "Player Two";
+            } else {
+                return "Tie";
+            }
         }
+    } else {
+        return null;
     }
-} else {
-    return null;
-}
 }
 
-function getGameWinner () {
+function getGameWinner() {
     // Placeholder code, replace with actual implementation
+
     let playerOneWins = 0;
     let playerTwoWins = 0;
-  for (let i = 1; i <= 3; i++) {
-   
-    if ( getRoundWinner(i) === 'Player One') {
-      playerOneWins += 1;
-    } else if ( getRoundWinner(i) === 'Player Two') {
-      playerTwoWins += 1;
+    for (let i = 1; i <= 3; i++) {
+        if (getRoundWinner(i)) {
+            if (getRoundWinner(i) === "Player One") {
+                playerOneWins += 1;
+            } else if (getRoundWinner(i) === "Player Two") {
+                playerTwoWins += 1;
+            }
+        } else {
+            return null;
+        }
     }
-  }
-  if (playerOneWins > playerTwoWins) {
-    return 'Player One';
-  } else if (playerOneWins < playerTwoWins) {
-    return 'Player Two';
-  } else {
-    return 'Tie';           
-  }
-
-   
+    if (playerOneWins > playerTwoWins) {
+        return "Player One";
+    } else if (playerOneWins < playerTwoWins) {
+        return "Player Two";
+    } else {
+        return "Tie";
+    }
 }
 
-function setComputerMoves() {   
-    // Add code here
-
+function setComputerMoves() {
+    
 }
